1a15d8fde4ed3ae2b923435c34ff6124edd26eb6
sclip <oscarkorpi@protonmail.com>
2023-08-04 17:17:25 +0200

    add: kotest dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,7 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
+val kotestVersion = "5.6"
+
 plugins {
     kotlin("jvm") version "1.9.0"
     kotlin("plugin.serialization") version "1.9.0"
@@ -31,11 +33,13 @@ dependencies {
     implementation("io.arrow-kt:arrow-core:1.2.0")
     implementation("io.arrow-kt:arrow-fx-coroutines:1.2.0")
 
-    implementation("com.zaxxer:HikariCP:5.0.1")  // Testing
+    // Testing
+    implementation("com.zaxxer:HikariCP:5.0.1")
     testImplementation(kotlin("test"))
-
-    // Not required for the library, only for testing
-    implementation("org.postgresql:postgresql:42.3.8")  // Postgres
+    implementation("io.kotest:kotest-runner-junit5:kotestVersion")
+    implementation("io.kotest:kotest-assertions-core:kotestVersion")
+    implementation("io.kotest:kotest-assertions-property:kotestVersion")
+    implementation("org.postgresql:postgresql:42.3.8")
     runtimeOnly("org.postgresql:postgresql")
 }
 
