c578c29330488b24054be6f89927ba0b0f0561ef
o-korpi <oscarkorpi@protonmail.com>
2023-08-05 13:37:38 +0200

    feat: add toSQLParseable for LocalDate

--- src/main/kotlin/io/query4k/serializers/toSQLParseable.kt
+++ src/main/kotlin/io/query4k/serializers/toSQLParseable.kt
@@ -1,9 +1,11 @@
 package io.query4k.serializers
 
+import kotlinx.datetime.LocalDate
 import kotlinx.datetime.LocalDateTime
 import kotlinx.datetime.LocalTime
 import kotlinx.uuid.UUID
 
 fun LocalTime.toSQLParseable() = this.toString()
+fun LocalDate.toSQLParseable() = this.toString()
 fun LocalDateTime.toSQLParseable() = this.toString()
 fun UUID.toSQLParseable(): String = "'${this}'"
