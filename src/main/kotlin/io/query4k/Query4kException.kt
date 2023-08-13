cd47cb4949230c05484fa8fd7a304cb51ccb28a1
o-korpi <oscarkorpi@protonmail.com>
2023-08-13 15:18:52 +0200

    feat: removed non-domain errors from QueryOnlyException

--- src/main/kotlin/io/query4k/Query4kException.kt
+++ src/main/kotlin/io/query4k/Query4kException.kt
@@ -2,7 +2,4 @@ package io.query4k
 
 interface Query4kException
 
-sealed class QueryOnlyException : Query4kException {
-    data object ConnectionException : QueryOnlyException()
-    data object IllegalStateException : QueryOnlyException()
-}
+data object QueryOnlyException : Query4kException
