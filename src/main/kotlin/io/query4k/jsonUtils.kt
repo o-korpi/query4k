b0a1e21af0883cc4745dc8fcefee41d5a00f8032
o-korpi <oscarkorpi@protonmail.com>
2023-08-13 22:21:47 +0200

    refactor: add internal annotations

--- src/main/kotlin/io/query4k/jsonUtils.kt
+++ src/main/kotlin/io/query4k/jsonUtils.kt
@@ -2,10 +2,10 @@ package io.query4k
 
 import kotlinx.serialization.json.*
 import kotlinx.serialization.serializer
+import org.jetbrains.annotations.ApiStatus
 
-// Functions for converting Any to JsonElement. Would've been private if it was possible with inline functions.
 
-
+@ApiStatus.Internal
 fun Any?.toJsonElement(): JsonElement = when (this) {
     is Number -> JsonPrimitive(this.toString())  // Convert all numbers to strings to make BigDecimal work
     is Boolean -> JsonPrimitive(this)
@@ -17,15 +17,22 @@ fun Any?.toJsonElement(): JsonElement = when (this) {
     else -> JsonPrimitive(this?.toString())
 }
 
+@ApiStatus.Internal
 fun Array<*>.toJsonArray() = JsonArray(map { it.toJsonElement() })
+
+@ApiStatus.Internal
 fun Iterable<*>.toJsonArray() = JsonArray(map { it.toJsonElement() })
+
+@ApiStatus.Internal
 fun Map<*, *>.toJsonObject() = JsonObject(mapKeys { it.key.toString() }.mapValues { it.value.toJsonElement() })
 
 //fun Json.encodeToString(vararg pairs: Pair<*, *>) = encodeToString(pairs.toMap().toJsonElement())
+@ApiStatus.Internal
 inline fun <reified A> Map<String, Any>.toType(): A = this
     .mapValues { it.value.toJsonElement() }
     .let { Json.encodeToString(serializer(), it) }
     .let { Json.decodeFromString<A>(it) }
 
 // TODO: this probably can't handle UUID? Test when possible
+@ApiStatus.Internal
 inline fun <reified A> Any.singleToType(): A = this.let { Json.decodeFromString<A>(this.toString()) }
